2ee70602000a34a953d1df7176e6ab6a3749b53e
Thanchanokcha <122966107+Thanchanokcha@users.noreply.github.com>
2023-02-25 00:18:46 +0700

    Add files via upload

--- system/history.php
+++ system/history.php
@@ -62,7 +62,8 @@
                     <?php if (isset($_SESSION['id'])) { ?>
                         <li class="nav-item"><a class="nav-link active" aria-current="page"> รหัสพนักงาน&nbsp;<?php echo $_SESSION['id']; ?>&nbsp;คุณ<?php echo $_SESSION['name']; ?></a></li>
 		                <?php }  ?>
-                        <li class="nav-item"><a class="nav-link" href="history.php">ประวัติการเข้างาน</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="history1.php">ประวัติการเข้างาน</a></li>
+                        <li class="nav-item"><a class="nav-link" href="history.php">ประวัติการลางาน</a></li>
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">ย้อนกลับ</a></li>
 			            <li class="nav-item"><a class="nav-link active" aria-current="page" href="login.php">Logout</a></li>
      			    <!-- <li class="active"><a href="admin_login.php">Admin</a></li> -->
--- system/index.php
+++ system/index.php
@@ -145,7 +145,8 @@
                         <?php if (isset($_SESSION['id'])) { ?>
                         <li class="nav-item"><a class="nav-link"> รหัสพนักงาน&nbsp;<?php echo $_SESSION['id']; ?>&nbsp;คุณ<?php echo $_SESSION['name']; ?></a></li>
 		                <?php }  ?>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="history.php">ประวัติการเข้างาน</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="history1.php">ประวัติการเข้างาน</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="history.php">ประวัติการลางาน</a></li>
 			            <li class="nav-item"><a class="nav-link active" aria-current="page" href="login.php">Logout</a></li>
                         <!-- <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li> -->
                         <!-- <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li> -->
